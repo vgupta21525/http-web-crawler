f9b99956fb8690869500f78b64e3e1ef70db8b6d
Vaibhav Gupta <vguptawork37@gmail.com>
2025-06-27 13:59:20 +0530

    Added single page crawling with error handling

--- crawl.ts
+++ crawl.ts
@@ -1,11 +1,40 @@
 import { JSDOM } from 'jsdom';
 
+async function crawlPage(currentURL: string) {
+    console.log(`Actively crawling: ${currentURL}`);
+    
+    try {
+        const response = await fetch(currentURL);
+        if (response.status > 399) {
+            console.log(`Error in fetch with status code: ${response.status} on page: ${currentURL}`);
+            return;
+        }
+
+        const contentType = response.headers.get('content-type');
+        if (!contentType?.includes('text/html')) {
+            console.log(`Non-HTML response, content-type: ${contentType} on page: ${currentURL}`);
+            return;
+        }
+
+
+        console.log(await response.text());
+    } catch (err) {
+        if (err instanceof Error) {
+            console.log(`Error while fetching ${currentURL}: ${err.message}`);
+        }
+        else {
+            console.log(`Unexpected error while fetching ${currentURL}: ${err}`);
+        }
+    }
+}
+
 function getURLsFromHTML(htmlBody: string, baseURL: string): string[] {
     const urls: string[] = [];
     const dom: JSDOM = new JSDOM(htmlBody);
     const linkElements: NodeListOf<HTMLAnchorElement> = dom.window.document.querySelectorAll('a');
     for (const linkElement of linkElements) {
         if (linkElement.href.slice(0, 1) === '/') {
+            // relative URL
             try {
                 const urlObj: URL = new URL(`${baseURL}${linkElement.href}`);
                 urls.push(urlObj.href);
@@ -17,6 +46,7 @@ function getURLsFromHTML(htmlBody: string, baseURL: string): string[] {
                 }
             }
         } else {
+            // absolute URL
             try {
                 const urlObj: URL = new URL(linkElement.href);
                 urls.push(linkElement.href);
@@ -42,4 +72,4 @@ function normalizeURL(urlString: string): string {
     return hostPath;
 }
 
-export { normalizeURL, getURLsFromHTML };
+export { normalizeURL, getURLsFromHTML, crawlPage };
--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+import { crawlPage } from "./crawl";
+
+function main(): void {
+    if (process.argv.length < 3) {
+        console.log('No website provided.');
+        process.exit(1);
+    } else if (process.argv.length > 3) {
+        console.log('Too many command line arguments.');
+        process.exit(1);
+    }
+
+    const baseURL: string = process.argv[2];
+    console.log(`Starting web crawler for ${baseURL}`);
+    crawlPage(baseURL);
+}
+
+main();
